9b31a3c3054e853cc66f6c73c25c4343913863c0
EgorTytskyi <129987284+EgorTytskyi@users.noreply.github.com>
2023-04-13 23:08:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import numpy as np
+import math
+from scipy.stats import t
 
+chat_id = 628156322 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control_NPV, test_NPV)-> bool:
+    alpha=0.09
+    n1 = len(control_NPV)
+    n2 = len(test_NPV)
+    mean1 = sum(control_NPV) / n1
+    mean2 = sum(test_NPV) / n2
+    var1 = sum((x - mean1)**2 for x in control_NPV) / (n1 - 1)
+    var2 = sum((x - mean2)**2 for x in test_NPV) / (n2 - 1)
+    s = math.sqrt((var1 + var2) / (n1 + n2 - 2))
+    t_stat = (mean1 - mean2) / (s * math.sqrt(1/n1 + 1/n2))
+    crit_val = t.ppf(1-alpha/2, n1+n2-2)
+    if abs(t_stat) > crit_val:
+        return True
+    else:
+        return False
